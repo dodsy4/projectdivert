54a0a521bcacec8bc183fa0b52105315d3d82a3f
dodsy4 <louisthomasdods@live.co.uk>
2026-02-15 11:32:52 +0000

    Normalize DATABASE_URL and support SQLAlchemy postgres dialect

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 import os
-SECRET_KEY = os.urandom(32)
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+DEBUG = os.environ.get('FLASK_DEBUG', '0') == '1'
 
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://louisdods@localhost:5432/pd'
+raw_database_url = os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
+if raw_database_url.startswith('postgres://'):
+    raw_database_url = raw_database_url.replace('postgres://', 'postgresql://', 1)
+
+SQLALCHEMY_DATABASE_URI = raw_database_url
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
